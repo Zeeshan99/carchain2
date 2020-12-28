c9cf7c8907cb58aba7db2181934bc0b33a46931a
starahmad6161 <starahmad616161@gmail.com>
2020-12-28 02:29:39 +0200

    add filter

--- js/find-car.js
+++ js/find-car.js
@@ -13,4 +13,36 @@ $(function() {
             }
         }
     });
+
+    loadSliderRange("#year-range");
+    loadSliderRange("#km-range");
+    loadSliderRange("#price-range");
+    
+    function loadSliderRange(sliderID) {
+        console.log(sliderID);
+        let parent = $(sliderID).parents(".range-box");
+        let minEle = parent.find(".min-txt");
+        let maxEle = parent.find(".max-txt");
+        let min = parseInt(minEle.data("min"));
+        let max = parseInt(maxEle.data("max"));
+
+        minEle.val(min);
+        maxEle.val(max);
+        $(sliderID).slider({
+            range: true,
+            min: min,
+            max: max,
+            values: [ min, max],
+            slide: function( event, ui ) {
+                parent.find(".min-txt").val(ui.values[ 0 ]);
+                parent.find(".max-txt").val(ui.values[ 1 ]);
+            }
+        });
+    }
+
+
+    $('#filterAccordion').collapse({
+        toggle: false
+    })
+
 });
